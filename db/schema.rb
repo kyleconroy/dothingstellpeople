3aa5be20204843bd1c8f74044a9a19c7fdd9cbdf
Ryan Bigg <radarlistener@gmail.com>
2012-02-28 12:20:00 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111112004339) do
+ActiveRecord::Schema.define(:version => 20120223175327) do
 
   create_table "forem_categories", :force => true do |t|
     t.string   "name",       :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "forem_forums", :force => true do |t|
@@ -25,28 +25,55 @@ ActiveRecord::Schema.define(:version => 20111112004339) do
     t.integer "category_id"
   end
 
+  create_table "forem_groups", :force => true do |t|
+    t.string "name"
+  end
+
+  add_index "forem_groups", ["name"], :name => "index_forem_groups_on_name"
+
+  create_table "forem_memberships", :force => true do |t|
+    t.integer "group_id"
+    t.integer "member_id"
+  end
+
+  add_index "forem_memberships", ["group_id"], :name => "index_forem_memberships_on_group_id"
+
+  create_table "forem_moderator_groups", :force => true do |t|
+    t.integer "forum_id"
+    t.integer "group_id"
+  end
+
+  add_index "forem_moderator_groups", ["forum_id"], :name => "index_forem_moderator_groups_on_forum_id"
+
   create_table "forem_posts", :force => true do |t|
     t.integer  "topic_id"
     t.text     "text"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.integer  "reply_to_id"
+    t.boolean  "pending_review", :default => true
   end
 
   add_index "forem_posts", ["reply_to_id"], :name => "index_forem_posts_on_reply_to_id"
   add_index "forem_posts", ["topic_id"], :name => "index_forem_posts_on_topic_id"
   add_index "forem_posts", ["user_id"], :name => "index_forem_posts_on_user_id"
 
+  create_table "forem_subscriptions", :force => true do |t|
+    t.integer "subscriber_id"
+    t.integer "topic_id"
+  end
+
   create_table "forem_topics", :force => true do |t|
     t.integer  "forum_id"
     t.integer  "user_id"
     t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "locked",     :default => false, :null => false
-    t.boolean  "pinned",     :default => false
-    t.boolean  "hidden",     :default => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+    t.boolean  "locked",         :default => false, :null => false
+    t.boolean  "pinned",         :default => false
+    t.boolean  "hidden",         :default => false
+    t.boolean  "pending_review", :default => true
   end
 
   add_index "forem_topics", ["forum_id"], :name => "index_forem_topics_on_forum_id"
@@ -77,7 +104,6 @@ ActiveRecord::Schema.define(:version => 20111112004339) do
     t.string   "last_sign_in_ip"
     t.string   "name"
     t.boolean  "forem_admin",                         :default => false
-    t.boolean  "boolean",                             :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
